3e9246630f3a7b75ef95c9582f57fab47ceb6366
Ian Li <ianarli@gmail.com>
2015-12-21 01:34:20 -0800

    Add Gulpfile and package.json

---
+++ Gulpfile.js
@@ -0,0 +1,28 @@
+/**
+ * Gulpfile for rsvp.harmonli.com
+ *
+ * @see http://tylermcginnis.com/reactjs-tutorial-pt-2-building-react-applications-with-gulp-and-browserify/
+ */
+
+var gae = require('gulp-gae');
+var gulp = require('gulp');
+
+var PORT = 14081;
+var ADMIN_PORT = 14082;
+
+gulp.task('gae_serve', function() {
+  gulp.src('./app.yaml')
+    .pipe(gae('dev_appserver.py', [], {
+      port: PORT,
+      admin_port: ADMIN_PORT
+    }));
+});
+
+gulp.task('gae_deploy', function() {
+  gulp.src('./app.yaml')
+    .pipe(gae('appcfg.py', ['update'], {
+      // appcfg.py parameters go here.
+    }));
+});
+
+gulp.task('default', ['gae_serve']);
